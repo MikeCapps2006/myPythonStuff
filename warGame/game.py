3c04ff51d8abc3fb81cdb9f1ed20d1e7b6e2a5cf
Mike Capps <michaelcapps1772@yahoo.com>
2024-01-29 06:42:43 -0500

    Finished game logic. Testing for bugs

--- warGame/game.py
+++ warGame/game.py
@@ -14,10 +14,10 @@ for x in range(26):
 
 def checkForLoss():
     if len(player_one.all_cards) == 0:
-        print('Player Two Wins!!!')
+        print('Player one has ran out of cards....Player Two Wins!!!')
         return True
     if len(player_two.all_cards) == 0:
-        print('Player One Wins!!!')
+        print('Player two has ran out of cards....Player One Wins!!!')
         return True
     return False
 
@@ -25,41 +25,60 @@ def checkForLoss():
 gameOn = True
 roundCount = 0
 while gameOn:
+    roundCount += 1
     player_one_cards_inPlay = []
     player_two_cards_inPlay = []
     roundCount += 1
-    if checkForLoss == True:
+    if checkForLoss():
         print('game loss')
+        print(f'Rounds - {roundCount}')
         gameOn = False
     else:
-        print('at war!!!')
         at_war = True
         player_one_cards_inPlay.append(player_one.remove_one())
         player_two_cards_inPlay.append(player_two.remove_one())
 
         while at_war:
             if player_one_cards_inPlay[0].value() > player_two_cards_inPlay[0].value():
+                print(player_one_cards_inPlay[0].value())
+                print(player_two_cards_inPlay[0].value())
                 print('player one wins this duel')
+                player_one.add_cards(player_one_cards_inPlay)
                 player_one.add_cards(player_two_cards_inPlay)
                 at_war = False
             elif player_two_cards_inPlay[0].value() > player_one_cards_inPlay[0].value():
                 print('player two wins this duel')
+                player_two.add_cards(player_two_cards_inPlay)
                 player_two.add_cards(player_one_cards_inPlay)
                 at_war = False
             else:
+                print('At War!!!')
+                print(player_one_cards_inPlay[0])
+                print(player_two_cards_inPlay[0])
                 if len(player_one.all_cards) < 5:
                     print('Player two wins!!!')
+                    print(f'Rounds - {roundCount}')
+                    player_two.add_cards(player_one_cards_inPlay)
+                    player_two.add_cards(player_one.all_cards)
+                    player_one.all_cards = []
+                    player_two.add_cards(player_two_cards_inPlay)
                     at_war = False
                     gameOn = False
                     break
-                if len(player_two.all_cards) < 5:
+                elif len(player_two.all_cards) < 5:
                     print('Player one wins!!!')
+                    print(f'Rounds - {roundCount}')
+                    player_one.add_cards(player_two_cards_inPlay)
+                    player_one.add_cards(player_two.all_cards)
+                    player_two.all_cards = []
+                    player_one.add_cards(player_one_cards_inPlay)
                     at_war = False
                     gameOn = False
                     break
-                for x in range(5):
-                    print('War in progress. Adding 5 more cards per person')
-                    player_one_cards_inPlay.append(player_one.remove_one())
-                    player_two_cards_inPlay.append(player_two.remove_one())
+                else:
+                    for x in range(5):
+                        print('War in progress. Adding 5 more cards per person')
+                        player_one_cards_inPlay.insert(0, player_one.remove_one())
+                        player_two_cards_inPlay.insert(0, player_two.remove_one())
 print(player_one)
 print(player_two)
